3c603bf4395cba380341e0268ec7539a38a18f80
uuu <498801920@qq.com>
2020-05-18 09:30:58 +0200

    lommeregner opgave
    test

---
+++ lommeregner/lommeregner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lommeregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write your first number");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("write your second number");
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("the result is "+ (tal1+tal2));
+        }
+    }
+}
